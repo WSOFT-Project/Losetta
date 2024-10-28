da272404c90dc907f0d245b7058a01909b9e6352
taiseiue <taisei@wsoft.ws>
2024-10-28 18:30:16 +0900

    結合テストを書く

--- Losetta.Tests/Losetta/Scripting/Statement/Others.cs
+++ Losetta.Tests/Losetta/Scripting/Statement/Others.cs
@@ -14,4 +14,24 @@ public class Others
             TestUtils.Script.Execute<Variable>(code);
         }, Throws.Nothing);
     }
+    [TestCase(Description = "よくあるAliceコードが一通り実行できる")]
+    public void CommonScript()
+    {
+        string code = @"
+        number a = 1;
+        number b = 2;
+        
+        number add(number x, number y)
+        {
+            return x + y;
+        }
+
+        var c = add(a, b);
+        print(c);
+
+        return c + 2;
+        ";
+        
+        Assert.That(TestUtils.Script.Execute<int>(code), Is.EqualTo(5));
+    }
 }
