f3b519addb8cb81db6e02c48f994b1e2001f0f98
taiseiue <taisei@wsoft.ws>
2024-12-28 16:53:08 +0900

    戻した

--- Losetta.Runtime/Core/Extension/Alice.Core.Dictionary.cs
+++ Losetta.Runtime/Core/Extension/Alice.Core.Dictionary.cs
@@ -76,11 +76,6 @@ namespace AliceScript.NameSpaces.Core
             return dict.Count;
         }
         [AliceFunction(Attribute = FunctionAttribute.LANGUAGE_STRUCTURE)]
-        public static int Capacity(this Dictionary<Variable, Variable> dict)
-        {
-            return dict.Capacity();
-        }
-        [AliceFunction(Attribute = FunctionAttribute.LANGUAGE_STRUCTURE)]
         public static int Size(this Dictionary<Variable, Variable> dict)
         {
             return dict.Count;
