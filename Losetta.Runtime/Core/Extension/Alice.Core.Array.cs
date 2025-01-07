9ac10ee2a0fd411ee1752cc6d81f0431aa6c7e51
taiseiue <taisei@wsoft.ws>
2025-01-07 11:48:45 +0900

    list.TrimExcess生やした

--- Losetta.Runtime/Core/Extension/Alice.Core.Array.cs
+++ Losetta.Runtime/Core/Extension/Alice.Core.Array.cs
@@ -441,6 +441,14 @@ namespace AliceScript.NameSpaces.Core
             throw new ScriptException("この実装では操作がサポートされていません", Exceptions.NOT_IMPLEMENTED);
 #endif
         }
+        public static int TrimExcess(this List<Variable> list)
+        {
+            return list.TrimExcess();
+        }
+        public static int TrimExcess(this List<Variable> list, int capacity)
+        {
+            return list.TrimExcess(capacity);
+        }
         #region 配列集計
         public static double Mean(this VariableCollection ary, [BindInfo] ParsingScript script, DelegateObject func)
         {
