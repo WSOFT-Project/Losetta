6594c04726190c167b05065a62f80ff9257cb33f
taiseiue <taiseiue@wsnet.jp>
2025-06-16 20:25:47 +0900

    ブレークポイントを作れるように

--- Losetta.Runtime/Alice.Diagnostics.cs
+++ Losetta.Runtime/Alice.Diagnostics.cs
@@ -217,6 +217,15 @@ namespace AliceScript.NameSpaces
             }
         }
         #endregion
+        #region デバッグ関連
+        public static void BreakPoint()
+        {
+            if (Interpreter.Instance.DebugMode)
+            {
+                throw new ScriptException(string.Empty, Exceptions.BREAK_POINT);
+            }
+        }
+        #endregion
     }
 
     [AliceObject(Name = "Process")]
