e3cbb4110daa14bda95f905a587f6507dcd70add
taiseiue <taiseiue@wsnet.jp>
2025-07-19 07:43:17 +0900

    for内で復合代入がバグる不具合を修正

--- Losetta/Constants.cs
+++ Losetta/Constants.cs
@@ -225,7 +225,7 @@ namespace AliceScript
         /// <summary>
         /// 複合代入式がとるパターン
         /// </summary>
-        public static readonly Regex COMPOUND_ASSIGN_PATTERN = new Regex(@"(.*)([\+\-\\\*\/\%\^\&\|\?])=(.*)", RegexOptions.Compiled);
+        public static readonly Regex COMPOUND_ASSIGN_PATTERN = new Regex(@"\s*(\w+)\s*([+\-*/%&|^]=)\s*(\w+)\s*", RegexOptions.Compiled);
 
         /// <summary>
         /// UTF16表現がとるパターン
